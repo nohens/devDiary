eba41fe5117b052e75df77ff49c742c6df30ce8e
Seo Seunghyun <tea7day@gmail.com>
2018-02-25 01:38:03 +0900

    modify sum_divisior test

--- seunghyun/algorithmn/python/sum_divisor_6_test.py
+++ seunghyun/algorithmn/python/sum_divisor_6_test.py
@@ -5,11 +5,8 @@ def sum_divisor(num):
     for i in range(1, num + 1):
         if num % i == 0:
             divisors.append(i)
-
-    print(f"result: {divisors}")
     return reduce(lambda x,y:x+y, divisors)
 
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
 def test_sum_divisor():
     assert sum_divisor(3) == 4
     assert sum_divisor(12) == 28
